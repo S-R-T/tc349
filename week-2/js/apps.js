590eb825b644d69d3f79b3ff6e31505c130ee9fd
Spencer Thulin <thulinsp@msu.edu>
2013-09-15 13:00:47 -0400

    Update apps.js

--- week-2/js/apps.js
+++ week-2/js/apps.js
@@ -13,17 +13,3 @@ if(sUsrAg.indexOf("Chrome") > -1) {
 }
 
 alert("You are using: " + sBrowser);
-
-function getBrowserId () {
-
-    var
-        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
-        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
-
-    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
-
-    return nIdx
-
-}
-
-console.log(getBrowserId());
