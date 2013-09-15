9a87fb410d5d002042ddaef2132f88a43dccefb9
Spencer Thulin <thulinsp@msu.edu>
2013-09-15 12:55:48 -0400

    Update apps.js

--- week-2/js/apps.js
+++ week-2/js/apps.js
@@ -1,3 +1,15 @@
-function hello() {
-  alert(navigator.appVersion);
+var Hello, sUsrAg = navigator.userAgent;
+
+if(sUsrAg.indexOf("Chrome") > -1) {
+    Hello = "Google Chrome";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    Hello = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+    Hello = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+    Hello = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+    Hello = "Microsoft Internet Explorer";
 }
+
+alert("You are using: " + Hello);
